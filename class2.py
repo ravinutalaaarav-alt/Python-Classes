baa61f22ac9a0aa09cfdb6785798d6c1a3562eee
ravinutalaaarav-alt <ravinutalaaarav@gmail.com>
2026-06-24 14:39:22 -0400

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,36 @@
+#activity 1
+print("welcome to the world of programming")
+print(4)
+#next line after print
+print("hello python \n")
+#pass multiple arguments for printing
+print("hello" ,5)
+#end argument of print statement
+print("welcome to ", end= "*")
+
+
+
+
+#activity 2
+#creating different variables
+x = 5
+y = "john"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#user input
+name = input("enter your name: ")
+
+print("\nhello", name, "\nwelcome to codingal" )
+
+
+
+#activity 3
+import keyword 
+
+# print all the python keywords
+print("python keywords are...\n")
+print(keyword.kwlist)
